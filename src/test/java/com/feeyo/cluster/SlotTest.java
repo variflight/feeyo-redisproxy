1fcd207eacaec4cd8d6078575f388e80f0aac8ff
yangtao <yangtao@variflight.com>
2017-09-20 14:45:06 +0800

    提交多了，还原

--- src/test/java/com/feeyo/cluster/SlotTest.java
+++ src/test/java/com/feeyo/cluster/SlotTest.java
@@ -1,10 +1,16 @@
 package com.feeyo.cluster;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.feeyo.redis.net.backend.pool.cluster.ClusterCRC16Util;
+
 
 public class SlotTest {
 
-//	@Test
-//	public void test() {
-//		assertEquals( ClusterCRC16Util.getSlot("key1", false), 9189 );
-//	}
+	@Test
+	public void test() {
+		assertEquals( ClusterCRC16Util.getSlot("key1", false), 9189 );
+	}
 
 }
