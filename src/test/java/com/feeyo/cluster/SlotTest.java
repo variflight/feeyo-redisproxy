c9763d9662d121b25be4f0a9af5bb0c49450d1e1
zhuam <zhuam@foxmail.com>
2017-10-17 10:51:08 +0800

    test case

--- src/test/java/com/feeyo/cluster/SlotTest.java
+++ src/test/java/com/feeyo/cluster/SlotTest.java
@@ -10,7 +10,7 @@ public class SlotTest {
 
 	@Test
 	public void test() {
-		assertEquals( ClusterCRC16Util.getSlot("key1", false), 9189 );
+		assertEquals( ClusterCRC16Util.getSlot("key1"), 9189 );
 	}
 
 }
