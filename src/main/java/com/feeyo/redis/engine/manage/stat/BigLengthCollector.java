d202236f19b6379a977803285f97aecc9baee084
zhuam <zhuam@foxmail.com>
2017-11-21 13:56:39 +0800

    重构

--- src/main/java/com/feeyo/redis/engine/manage/stat/BigLengthCollector.java
+++ src/main/java/com/feeyo/redis/engine/manage/stat/BigLengthCollector.java
@@ -1,6 +1,5 @@
 package com.feeyo.redis.engine.manage.stat;
 
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,16 +21,16 @@ public class BigLengthCollector implements StatCollector {
 	
 	private static Logger LOGGER = LoggerFactory.getLogger( BigLengthCollector.class );
 	
-	private final static int LENGTH_THRESHOLD = 10000;
+	private final static int THRESHOLD = 10000;
 	
-	private final static int MIN_WATCH_LEN = 500;
-	private final static int MAX_WATCH_LEN = 1000;
+	private final static int SIZE_OF_1K = 1024;
+	private final static int SIZE_OF_10K = 10 * SIZE_OF_1K;
 	
-	// 缓存最近出现过的集合类型key
-	private static ConcurrentHashMap<String, BigLength> ckeyMap = new ConcurrentHashMap<String, BigLength>();
-	private static ConcurrentHashMap<String, BigLength> ckeyTop100Map = new ConcurrentHashMap<String, BigLength>();
 	
-
+	// key -> password,cmd
+	private static ConcurrentHashMap<String, String[]> keyMap = new ConcurrentHashMap<String, String[]>();
+	
+	private static ConcurrentHashMap<String, BigLength> bigLengthMap = new ConcurrentHashMap<String, BigLength>();
 	
 	private static long lastCheckTime = TimeUtil.currentTimeMillis();
 	private static AtomicBoolean isChecking = new AtomicBoolean(false);
@@ -41,7 +40,7 @@ public class BigLengthCollector implements StatCollector {
 	/**
 	 * 检查 redis key
 	 */
-	private void checkKeyLength() {
+	private void checkListKeyLength() {
 		
 		if ( !isChecking.compareAndSet(false, true) ) {
 			return;
@@ -51,9 +50,15 @@ public class BigLengthCollector implements StatCollector {
 
 			lastCheckTime = TimeUtil.currentTimeMillis();
 			
-			for (BigLength cKey : ckeyMap.values()) {
+			
+			for (java.util.Map.Entry<String, String[]>  listKey : keyMap.entrySet()) {
 				
-				UserCfg userCfg = RedisEngineCtx.INSTANCE().getUserMap().get( cKey.password );
+				String key = listKey.getKey();
+				String[] value = listKey.getValue();
+				String password = value[0];
+				String cmd = value[1];
+				
+				UserCfg userCfg = RedisEngineCtx.INSTANCE().getUserMap().get( password );
 				
 				if (userCfg != null) {
 					AbstractPool pool = RedisEngineCtx.INSTANCE().getPoolMap().get( userCfg.getPoolId() );
@@ -67,68 +72,63 @@ public class BigLengthCollector implements StatCollector {
 						conn.sendCommand( RedisCommand.READONLY );
 						conn.getBulkReply();
 						
-						String cmd = cKey.cmd;
 						if ( cmd.equals("HMSET") 	
 								|| cmd.equals("HSET") 	
 								|| cmd.equals("HSETNX") 	
 								|| cmd.equals("HINCRBY") 	
 								|| cmd.equals("HINCRBYFLOAT") 	) { // hash
 							
-							conn.sendCommand( RedisCommand.HLEN, cKey.key );
+							conn.sendCommand( RedisCommand.HLEN, key );
 							
 						} else if (cmd.equals("LPUSH") 	
 								|| cmd.equals("LPUSHX") 
 								|| cmd.equals("RPUSH") 
 								|| cmd.equals("RPUSHX") ) { // list
 							
-							conn.sendCommand( RedisCommand.LLEN, cKey.key );
+							conn.sendCommand( RedisCommand.LLEN, key );
 							
 						} else if(  cmd.equals("SADD") ) {  // set
 								
-								conn.sendCommand( RedisCommand.SCARD, cKey.key );
+								conn.sendCommand( RedisCommand.SCARD, key );
 							
 						} else if ( cmd.equals("ZADD")  
 								|| cmd.equals("ZINCRBY") 
 								|| cmd.equals("ZREMRANGEBYLEX")) { // sortedset
 							
-							conn.sendCommand( RedisCommand.ZCARD, cKey.key );
+							conn.sendCommand( RedisCommand.ZCARD, key );
 						}
 						
 						// 获取集合长度
 						long length = conn.getIntegerReply();
-						if ( length > LENGTH_THRESHOLD ) {
+						if ( length > THRESHOLD ) {
 							
-							cKey.length.set((int) length);
-
-							if (ckeyTop100Map.get(cKey.key) != null) {
-								BigLength ck = ckeyTop100Map.get(cKey.key);
-								ck.length = cKey.length;
-								
-							} else if (ckeyTop100Map.size() > 100) {
-								
-								BigLength minCKey = null;
-								for (Entry<String, BigLength> entry1 : ckeyTop100Map.entrySet()) {
-									BigLength ck = entry1.getValue();
-									if ( minCKey == null ) {
-										minCKey = ck;
-									} else {
-										if (ck.length.get() < minCKey.length.get()) {
-											minCKey = ck;
-										}
-									}
-								}
-								
-								if ( minCKey != null)
-									ckeyTop100Map.remove(minCKey.key);
-								
-							} else {
-								ckeyTop100Map.put(cKey.key, cKey);
+							BigLength bigLen = bigLengthMap.get(key);
+							if ( bigLen == null ) {
+								bigLen = new BigLength();
+								bigLen.cmd = cmd;
+								bigLen.key = key;
 							}
-
+							bigLen.length.set( (int)length );
+							
 						} else {
-
-							ckeyTop100Map.remove(cKey.key);
+							keyMap.remove( key );
 						}
+						
+						
+						//###########################################
+						BigLength min = null;
+						for (BigLength bigLen : bigLengthMap.values()) {
+							if ( min == null ) {
+								min = bigLen;
+							} else {
+								if (bigLen.length.get() < min.length.get()) {
+									min = bigLen;
+								}
+							}
+						}
+						if ( min != null)
+							bigLengthMap.remove( min.key );
+						
 						
 					} catch (JedisDataException e1) {
 					} catch (JedisConnectionException e2) {
@@ -142,6 +142,7 @@ public class BigLengthCollector implements StatCollector {
 				}
 			}
 			
+			
 		} finally {
 			isChecking.set(false);
 		}
@@ -149,9 +150,11 @@ public class BigLengthCollector implements StatCollector {
 
 	    
 	public ConcurrentHashMap<String, BigLength> getBigLengthMap() {
-		return ckeyTop100Map;
+		return bigLengthMap;
 	}
 	
+	
+
 	
 	@Override
 	public void onCollect(String password, String cmd, String key, int requestSize, int responseSize, 
@@ -175,30 +178,24 @@ public class BigLengthCollector implements StatCollector {
 				|| cmd.equals("ZINCRBY") 
 				|| cmd.equals("ZREMRANGEBYLEX") ) {
 		
-		
 	
-				// 如果是写入指令，并且此数据是集合类key长度top100中。判断此次请求的数据大小，记录，用于估算大长度key的数据大小。
-				BigLength ck = ckeyTop100Map.get(key);
-				if (ck != null) {
-					if (requestSize > 10 * 1024) {
-						ck.count_10k.incrementAndGet();
-					} else if (requestSize > 1024) {
-						ck.count_1k.incrementAndGet();
+				BigLength bigLength = bigLengthMap.get(key);
+				if (bigLength != null) {
+					if (requestSize > SIZE_OF_10K ) {
+						bigLength.count_10k.incrementAndGet();
+						
+					} else if (requestSize > SIZE_OF_1K) {
+						bigLength.count_1k.incrementAndGet();
 					}
 				}
 				
-				BigLength cKey = ckeyMap.get(key);
-				if (cKey == null) {
-					if (ckeyMap.size() < MAX_WATCH_LEN) {
-						cKey = new BigLength();
-						cKey.cmd = cmd;
-						cKey.key = key;
-						cKey.password = password;
-						ckeyMap.put(key, cKey);
+				if ( !keyMap.containsKey(key) ) {
+					
+					if (keyMap.size() < 1000) {;
+						keyMap.put(key, new String[]{ password, cmd});
+					} else {
+						checkListKeyLength();
 					}
-					if (ckeyMap.size() >= MIN_WATCH_LEN 
-							&& isChecking.compareAndSet(false, true)) 
-						checkKeyLength();
 				}
 		}
 		
@@ -211,9 +208,8 @@ public class BigLengthCollector implements StatCollector {
 	
 	@Override
 	public void onSchedulePeroid(int peroid) {
-		
 		if (TimeUtil.currentTimeMillis() - lastCheckTime >= peroid * 1000 ) {
-			checkKeyLength();
+			checkListKeyLength();
         }
 	}
 	
