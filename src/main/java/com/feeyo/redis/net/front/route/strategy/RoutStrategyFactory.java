01b0a7d2d2fee5aad562cc1c2f95c8ed030b1ecc
zhuam <zhuam@foxmail.com>
2017-11-23 09:51:52 +0800

    重构

--- src/main/java/com/feeyo/redis/net/front/route/strategy/RoutStrategyFactory.java
+++ src/main/java/com/feeyo/redis/net/front/route/strategy/RoutStrategyFactory.java
@@ -11,12 +11,14 @@ public class RoutStrategyFactory {
 	private static SegmentRoutStrategy  _SEGMENT = new SegmentRoutStrategy();
 	
     public static AbstractRouteStrategy getStrategy(int poolType, boolean isNeedSegment) {
-    	
     	// 集群情况下，需要对 Mset、Mget、Del mulitKey 分片
-    	if ((poolType == 1 || poolType == 2) && isNeedSegment) {
+    	switch( poolType ) {
+    	case 1:
+    	case 2:
+    		if ( isNeedSegment )
     			 return _SEGMENT;
+    		break;
     	}
-    	
     	return _DEFAULT;
     }
 
