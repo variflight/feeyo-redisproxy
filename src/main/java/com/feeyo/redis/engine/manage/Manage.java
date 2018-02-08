3c28f1f8d26cebc6771d0c1c004e1097e8b1871a
yangtao <yangtao@variflight.com>
2018-02-08 11:31:29 +0800

    增加管理指令

--- src/main/java/com/feeyo/redis/engine/manage/Manage.java
+++ src/main/java/com/feeyo/redis/engine/manage/Manage.java
@@ -119,6 +119,7 @@ public class Manage {
 	 *  SHOW POOL
 	 *  SHOW COST
 	 *  SHOW USER_DAY_NET_IO
+	 *  SHOW POOL_NET_IO POOLNAME
 	 *  
 	 *  SHOW LOG_ERROR
 	 *  SHOW LOG_WARN
@@ -585,6 +586,59 @@ public class Manage {
 						sb.append(entry.getKey()).append(":").append(entry.getValue().get()).append(". ");
 					}
 					lines.add(sb.toString());
+					
+					return encode( lines );
+				
+				// SHOW POOL_NET_IO POOLNAME
+				} else if ( arg2.equalsIgnoreCase("POOL_NET_IO")  && numArgs == 3 ) {
+					
+					// TODO
+					String poolName = new String( request.getArgs()[2] );
+					
+					List<String> lines = new ArrayList<String>();
+					
+					Map<String, AtomicInteger> poolConnections = new HashMap<String, AtomicInteger>();
+					ConcurrentMap<Long, Connection> allConnections = NetSystem.getInstance().getAllConnectios();
+					Iterator<Entry<Long, Connection>> it = allConnections.entrySet().iterator();
+					long minStartupTime = -1;
+					long maxLastLargeMessageTime = -1;
+					long totalLargeCount = 0;
+					long totalNetInCount = 0;
+					long totalNetInBytes = 0;
+					long totalNetOutBytes = 0;
+					
+					while (it.hasNext()) {
+						Connection c = it.next().getValue();
+						if ( c instanceof RedisBackendConnection ) {
+							// 统计每个redis池的连接数 
+							if (((RedisBackendConnection) c).getPhysicalNode().getPoolName().equals(poolName)) {
+								StringBuffer sb = new StringBuffer();
+								sb.append("ID=").append(c.getId()).append(". ");
+								sb.append("StartupTime=").append(c.getStartupTime()).append(". ");
+								sb.append("LastLargeMessageTime=").append(c.getLastLargeMessageTime()).append(". ");
+								sb.append("LargeCount=").append(c.getLargeCount()).append(". ");
+								sb.append("NetInCount=").append(c.getNetInCount()).append(". ");
+								sb.append("NetInBytes=").append(c.getNetInBytes()).append(". ");
+								sb.append("NetOutBytes=").append(c.getNetOutBytes()).append(". ");
+								lines.add( sb.toString() );
+								
+								minStartupTime = minStartupTime < 0 ? c.getStartupTime() : Math.min(minStartupTime, c.getStartupTime());
+								maxLastLargeMessageTime = Math.max(maxLastLargeMessageTime, c.getLastLargeMessageTime());
+								totalLargeCount = totalLargeCount + c.getLargeCount();
+								totalNetInCount = totalNetInCount + c.getNetInCount();
+								totalNetInBytes = totalNetInBytes + c.getNetInBytes();
+								totalNetOutBytes = totalNetOutBytes + c.getNetOutBytes();
+							}
+						}
+					}
+					StringBuffer end = new StringBuffer();
+					end.append("MinStartupTime=").append(minStartupTime).append(". ");
+					end.append("MaxLastLargeMessageTime=").append(maxLastLargeMessageTime).append(". ");
+					end.append("TotalLargeCount=").append(totalLargeCount).append(". ");
+					end.append("TotalNetInCount=").append(totalNetInCount).append(". ");
+					end.append("TotalNetInBytes=").append(totalNetInBytes).append(". ");
+					end.append("TotalNetOutBytes=").append(totalNetOutBytes).append(". ");
+					lines.add(end.toString());
 					
 					return encode( lines );
 					
