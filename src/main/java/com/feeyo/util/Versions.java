e30272da5177a1fa443792f0035de77f7a2f2eb3
yangtao <yangtao@variflight.com>
2018-03-14 11:38:51 +0800

    sheng ji

--- src/main/java/com/feeyo/util/Versions.java
+++ src/main/java/com/feeyo/util/Versions.java
@@ -2,7 +2,7 @@ package com.feeyo.util;
 
 public class Versions {
 	
-	public static String SERVER_VERSION = "redis-proxy-1.8-20180211_V2";
+	public static String SERVER_VERSION = "redis-proxy-1.8-20180314";
 
 
 }
