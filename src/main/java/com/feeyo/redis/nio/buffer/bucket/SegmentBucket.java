77137aac40f8c9f57fb97f7225e41ce6e2bbc060
zhuam <zhuam@foxmail.com>
2018-03-28 14:32:27 +0800

    remove threadlocal pool

--- src/main/java/com/feeyo/redis/nio/buffer/bucket/SegmentBucket.java
+++ src/main/java/com/feeyo/redis/nio/buffer/bucket/SegmentBucket.java
@@ -1,32 +1,23 @@
 package com.feeyo.redis.nio.buffer.bucket;
 
 import java.nio.ByteBuffer;
-import java.util.Collection;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.feeyo.redis.nio.buffer.BufferPool;
-
 public class SegmentBucket extends AbstractBucket {
 	
-	private final ConcurrentLinkedQueue<ByteBuffer>[] buffers;
+	private final ConcurrentLinkedQueue<ByteBuffer>[] childQueue;
 	
-	private final static int buffersSize = 16;
-	private final AtomicInteger allocateIndex = new AtomicInteger(0);
-	private final AtomicInteger recycleIndex = new AtomicInteger(0);
-	
-	private final ThreadLocalBufferPool localBufferPool;
-	private int threadLocalCount;
-	private final int localBufThreadPrexLength;
+	private final AtomicInteger pollIdx = new AtomicInteger(0);
+	private final AtomicInteger offerIdx = new AtomicInteger(0);
 
-	
 	@SuppressWarnings("unchecked")
 	public SegmentBucket(BucketBufferPool pool, int chunkSize, int count, boolean isExpand, int threadLocalPercent) {
 		super(pool, chunkSize, count, isExpand, threadLocalPercent);
 
-		this.buffers = new ConcurrentLinkedQueue[buffersSize];
-		for (int i = 0; i < buffers.length; i++) {
-			this.buffers[i] = new ConcurrentLinkedQueue<ByteBuffer>();
+		this.childQueue = new ConcurrentLinkedQueue[16];
+		for (int i = 0; i < childQueue.length; i++) {
+			this.childQueue[i] = new ConcurrentLinkedQueue<ByteBuffer>();
 		}
 		
 		// 初始化
@@ -34,11 +25,6 @@ public class SegmentBucket extends AbstractBucket {
 			queueOffer(ByteBuffer.allocateDirect(chunkSize));
 			pool.getUsedBufferSize().addAndGet(chunkSize);
 		}
-		
-		this.localBufThreadPrexLength = BufferPool.LOCAL_BUF_THREAD_PREX.length();
-		this.threadLocalCount = count * threadLocalPercent / 100;
-		
-		this.localBufferPool = new ThreadLocalBufferPool(threadLocalCount);
 	}
 
 	@Override
@@ -53,43 +39,19 @@ public class SegmentBucket extends AbstractBucket {
 
 	@Override
 	protected boolean queueOffer(ByteBuffer buffer) {
-		
-		if (isLocalCacheThread()) {
-			BufferQueue localQueue = localBufferPool.get();
-			if (localQueue.snapshotSize() < 100) {
-				localQueue.put(buffer);
-			} else {
-				// recyle 3/4 thread local buffer
-				Collection<ByteBuffer> buffers = localQueue.removeItems(threadLocalCount * 1 / 4);
-				for (ByteBuffer buf : buffers) {
-					this.buffers[getIndex(recycleIndex)].offer( buf );
-				}
-				this.buffers[getIndex(recycleIndex)].offer( buffer );
-			}
-			return true;
-		}
-		
-		return this.buffers[getIndex(recycleIndex)].offer( buffer );
+		ConcurrentLinkedQueue<ByteBuffer> queue = this.childQueue[ getNextIndex(offerIdx) ];
+		return queue.offer( buffer );
 	}
 
 	@Override
 	protected ByteBuffer queuePoll() {
-		
-		// 从线程的二级缓存 获取
-		if ( isLocalCacheThread() ) {
-			// allocate from threadlocal
-			ByteBuffer bb = localBufferPool.get().poll();
-			if (bb != null) {
-				return bb;
-			}
-		}
-		
-		return (ByteBuffer) this.buffers[getIndex(allocateIndex)].poll();
+		ConcurrentLinkedQueue<ByteBuffer> queue = this.childQueue[ getNextIndex(pollIdx) ];
+		return queue.poll();
 	}
 
 	@Override
 	protected void containerClear() {
-		for (ConcurrentLinkedQueue<ByteBuffer> cq : buffers) {
+		for (ConcurrentLinkedQueue<ByteBuffer> cq : childQueue) {
 			cq.clear();
 		}
 	}
@@ -97,25 +59,18 @@ public class SegmentBucket extends AbstractBucket {
 	@Override
 	public int getQueueSize() {
 		int size = 0;
-		for (ConcurrentLinkedQueue<ByteBuffer> cq : buffers) {
+		for (ConcurrentLinkedQueue<ByteBuffer> cq : childQueue) {
 			size = size + cq.size();
 		}
 		return size;
 	}
 	  
-	private final int getIndex(AtomicInteger ai) {
+	private final int getNextIndex(AtomicInteger atomic) {
 		for (;;) {
-			int current = ai.get();
+			int current = atomic.get();
 			int next = current >= 15 ? 0 : current + 1;
-			if (ai.compareAndSet(current, next))
+			if (atomic.compareAndSet(current, next))
 				return current;
 		}
 	}
-	
-	private final boolean isLocalCacheThread() {
-		final String thname = Thread.currentThread().getName();
-		return (thname.length() < localBufThreadPrexLength) ? false
-				: (thname.charAt(0) == '$' && thname.charAt(1) == '_');
-	}
-
 }
