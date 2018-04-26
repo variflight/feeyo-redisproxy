6a01b367165e3d7a4853353fa8a6068e9309b3e0
zhuam <zhuam@foxmail.com>
2018-04-26 11:51:38 +0800

    rt

--- src/main/java/com/feeyo/kafka/protocol/CommonFields.java
+++ src/main/java/com/feeyo/kafka/protocol/CommonFields.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.feeyo.kafka.protocol;
 
 import com.feeyo.kafka.protocol.types.Field;
@@ -22,6 +6,7 @@ public class CommonFields {
     public static final Field.Int32 THROTTLE_TIME_MS = new Field.Int32("throttle_time_ms",
             "Duration in milliseconds for which the request was throttled due to quota violation (Zero if the " +
                     "request did not violate any quota)", 0);
+    
     public static final Field.Str TOPIC_NAME = new Field.Str("topic", "Name of topic");
     public static final Field.Int32 PARTITION_ID = new Field.Int32("partition", "Topic partition id");
     public static final Field.Int16 ERROR_CODE = new Field.Int16("error_code", "Response error code");
