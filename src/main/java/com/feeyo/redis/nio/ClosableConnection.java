43e7633a482bc6614d8c3480db06de12619511d2
zhuam <zhuam@foxmail.com>
2018-04-26 11:49:44 +0800

    rt

--- src/main/java/com/feeyo/redis/nio/ClosableConnection.java
+++ src/main/java/com/feeyo/redis/nio/ClosableConnection.java
@@ -2,25 +2,19 @@ package com.feeyo.redis.nio;
 
 public interface ClosableConnection {
 	
-	/**
-	 * 关闭连接
-	 */
+	//关闭连接
 	void close(String reason);
-
 	boolean isClosed();
-
+	
 	public void idleCheck();
 
 	long getStartupTime();
 
 	String getHost();
-
 	int getPort();
-
 	int getLocalPort();
 
 	long getNetInBytes();
-
 	long getNetOutBytes();
 
 	// 限流 ( 限流开关、流量清洗 )
