44952d50cbedf9647b151f94ca20952ab8cf7bda
zhuam <zhuam@foxmail.com>
2018-05-28 17:12:42 +0800

    rt

--- src/main/java/com/feeyo/kafka/net/backend/broker/zk/ZkClientx.java
+++ src/main/java/com/feeyo/kafka/net/backend/broker/zk/ZkClientx.java
@@ -1,6 +1,7 @@
 package com.feeyo.kafka.net.backend.broker.zk;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.I0Itec.zkclient.IZkConnection;
 import org.I0Itec.zkclient.ZkClient;
@@ -11,37 +12,28 @@ import org.I0Itec.zkclient.exception.ZkNodeExistsException;
 import org.I0Itec.zkclient.serialize.ZkSerializer;
 import org.apache.zookeeper.CreateMode;
 
-import com.google.common.base.Function;
-import com.google.common.collect.MigrateMap;
-
 /**
  * 使用自定义的ZooKeeperx for zk connection
  */
 public class ZkClientx extends ZkClient {
 
     // 对于zkclient进行一次缓存，避免一个jvm内部使用多个zk connection
-	private static Map<String, ZkClientx> clients = MigrateMap.makeComputingMap(new Function<String, ZkClientx>() {
-		public ZkClientx apply(String servers) {
-			return new ZkClientx(servers);
-		}
-	});
+	private static Map<String, ZkClientx> clients = new ConcurrentHashMap<String, ZkClientx>();
 
 	private static Object _lock = new Object();
 
 	public static ZkClientx getZkClient(String servers) {
-		
-		 return clients.get(servers);
-		
-//		ZkClientx zkClientx = clients.get(servers);
-//		if (zkClientx == null) {
-//			synchronized (_lock) {
-//				if (zkClientx == null) {
-//					zkClientx = new ZkClientx(servers);
-//					clients.put(servers, zkClientx);
-//				}
-//			}
-//		}
-//		return zkClientx;
+
+		ZkClientx zkClientx = clients.get(servers);
+		if (zkClientx == null) {
+			synchronized (_lock) {
+				if (zkClientx == null) {
+					zkClientx = new ZkClientx(servers);
+					clients.put(servers, zkClientx);
+				}
+			}
+		}
+		return zkClientx;
 	}
 
     public ZkClientx(String serverstring){
