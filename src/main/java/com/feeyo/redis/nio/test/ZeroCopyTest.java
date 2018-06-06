90372c9612dc3441643d4ad417ac2b96f8b08297
zhuam <zhuam@foxmail.com>
2018-06-06 18:14:07 +0800

    rt

--- src/main/java/com/feeyo/redis/nio/test/ZeroCopyTest.java
+++ src/main/java/com/feeyo/redis/nio/test/ZeroCopyTest.java
@@ -6,6 +6,7 @@ import com.feeyo.redis.nio.ConnectionFactory;
 import com.feeyo.redis.nio.NIOAcceptor;
 import com.feeyo.redis.nio.NIOReactorPool;
 import com.feeyo.redis.nio.NetSystem;
+import com.feeyo.redis.nio.SystemConfig;
 import com.feeyo.redis.nio.buffer.BufferPool;
 import com.feeyo.redis.nio.buffer.bucket.BucketBufferPool;
 import com.feeyo.util.ExecutorUtil;
@@ -23,6 +24,9 @@ public class ZeroCopyTest {
        
         
         new NetSystem(bufferPool, ExecutorUtil.create("BusinessExecutor-", 2), ExecutorUtil.create("TimerExecutor-", 2));
+        
+        SystemConfig systemConfig = new SystemConfig();
+        NetSystem.getInstance().setNetConfig( systemConfig );
 		
 		String name = "zerocopy";
 		String bindIp = "0.0.0.0";
