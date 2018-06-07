f969da4a11b4c2a67415789d7dfe15b769597da2
zhuam <zhuam@foxmail.com>
2018-06-07 16:12:54 +0800

    rt

--- src/main/java/com/feeyo/redis/net/front/bypass/BypassService.java
+++ src/main/java/com/feeyo/redis/net/front/bypass/BypassService.java
@@ -77,6 +77,7 @@ public class BypassService {
 		StatUtil.getBigKeyCollector().setBigkeySize( requireSize );
 	}
 	
+	// 检测
 	public boolean testing(String requestCmd, byte[] requestKey, int requestSize) {
 		
 		if ( requestSize >= requireSize || StatUtil.getBigKeyCollector().isResponseBigkey(new String(requestKey), requestCmd)) {
@@ -86,13 +87,7 @@ public class BypassService {
 		return false;
 	}
 
-	// 单例
-	// 固定 coreSize maxSize queue threadPool
-	// front to backend 同步请求
-	// 支持  bigkey size 设置的能力、 reload 能力
-	// 支持 request & response bigkey 检测 ， response bigkey, 需要 ttl scan 
-	// 第一步支持 单请求， 后续考虑支持 pipeline 等 
-	// 
+	// 排队执行
 	public void queuing(final RedisRequest request, final RedisFrontConnection frontConn, final PhysicalNode physicalNode) {
 		
 		try {
