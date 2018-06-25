ca1a5303c7da15267deda26d03f718e45fe9334f
zhuam <zhuam@foxmail.com>
2018-06-25 11:52:38 +0800

    rt

--- src/main/java/com/feeyo/net/nio/NetFlowMonitor.java
+++ src/main/java/com/feeyo/net/nio/NetFlowMonitor.java
@@ -1,7 +1,10 @@
 package com.feeyo.net.nio;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+import com.feeyo.net.nio.util.TimeUtil;
 import com.feeyo.redis.config.NetFlowCfg;
 
 
@@ -11,27 +14,110 @@ import com.feeyo.redis.config.NetFlowCfg;
  */
 public class NetFlowMonitor {
 	
-  	private volatile Map<String, NetFlowCfg> netflowCfgMap;
+  	private volatile Map<String, NetFlowControl> ctrlMap = null;
   	
-	public void setCfgs(Map<String, NetFlowCfg> cfgs) {
-		this.netflowCfgMap = cfgs;
+	public synchronized void setCfgs(Map<String, NetFlowCfg> cfgMap) {
+		
+		if ( cfgMap != null ) {
+			
+			Map<String, NetFlowControl> tmpCtrlMap = new HashMap<String, NetFlowControl>();
+			
+			for(Map.Entry<String, NetFlowCfg>  entry : cfgMap.entrySet() ) {
+				String pwd = entry.getKey();
+				NetFlowCfg cfg = entry.getValue();
+				
+				if ( cfg != null && cfg.isControl() ) {
+					NetFlowControl  ctrl = new NetFlowControl( cfg );
+					tmpCtrlMap.put(pwd, ctrl);
+				}
+				
+			}
+			
+			this.ctrlMap = tmpCtrlMap;
+		}
 	}
 	
 	//
 	public boolean pool(String user, long length) {
 		
-		if ( netflowCfgMap == null || netflowCfgMap.isEmpty() ) {
+		if ( ctrlMap == null || ctrlMap.isEmpty() ) {
 			return false;
 		}
 		
 		//
-		NetFlowCfg cfg = netflowCfgMap.get(user);
-		if ( cfg != null && cfg.isControl() ) {
-			//
-			return cfg.pool( length );
+		NetFlowControl control = ctrlMap.get(user);
+		if ( control != null ) {
+			return control.pool( length );
 		}
 		
 		return false;
+	}
+	
+	
+	//
+	class NetFlowControl {
+		
+		private int perSecondMaxSize;
+		private int requestMaxSize;
+		//
+		private int currentIndex;
+		private AtomicLong[] sencondSizes;
+		
+		public NetFlowControl(NetFlowCfg cfg) {
+			
+			this.perSecondMaxSize = cfg.getPerSecondMaxSize();
+			this.requestMaxSize = cfg.getRequestMaxSize();
+			
+			//
+			this.sencondSizes = new AtomicLong[60];
+			for (int i = 0; i < sencondSizes.length; i++) {
+				this.sencondSizes[i] = new AtomicLong( perSecondMaxSize );
+			}
+		}
+		
+		/**
+		 * @param length
+		 * @return 是否超出流量
+		 */
+		public boolean pool(long length) {
+			
+			if ( length > requestMaxSize) {
+				return true;
+			}
+
+			if (this.perSecondMaxSize > 0 && length > 0) {
+
+				long currentTimeMillis = TimeUtil.currentTimeMillis();
+				// long currentTimeMillis = System.currentTimeMillis();
+
+				int tempIndex = (int) ((currentTimeMillis / 1000) % 60);
+				if (currentIndex != tempIndex) {
+					synchronized (this) {
+						// 这一秒的第一条统计，把对应的存储位的数据置是 max
+						if (currentIndex != tempIndex) {
+							
+							// reset
+							sencondSizes[tempIndex].set(perSecondMaxSize);
+							currentIndex = tempIndex;
+						}
+					}
+				}
+
+				return decrement(sencondSizes[currentIndex], length) <= 0;
+			}
+
+			return true;
+		}
+		
+		private final long decrement(AtomicLong atomicLong, long delta) {
+			for (;;) {
+				long current = atomicLong.get();
+				long next = current - delta;
+				if (atomicLong.compareAndSet(current, next))
+					return next;
+			}
+		}
+		
 	}
    
 }
--- src/main/java/com/feeyo/redis/config/NetFlowCfg.java
+++ src/main/java/com/feeyo/redis/config/NetFlowCfg.java
@@ -1,19 +1,11 @@
 package com.feeyo.redis.config;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import com.feeyo.net.nio.util.TimeUtil;
-
 public class NetFlowCfg {
 
 	private final String password;
 	private final int perSecondMaxSize;
 	private final int requestMaxSize;
 	private boolean isControl;
-	
-	//
-	private int currentIndex;
-	private AtomicLong[] sencondSizes;
 
 	public NetFlowCfg(String password, int perSecondMaxSize, int requestMaxSize, boolean isControl) {
 		this.password = password;
@@ -21,47 +13,6 @@ public class NetFlowCfg {
 		this.requestMaxSize = requestMaxSize;
 		this.isControl = isControl;
 
-		if (this.perSecondMaxSize > 0) {
-
-			sencondSizes = new AtomicLong[60];
-			for (int i = 0; i < sencondSizes.length; i++) {
-				sencondSizes[i] = new AtomicLong( perSecondMaxSize );
-			}
-		}
-	}
-
-	/**
-	 * @param length
-	 * @return 是否超出流量
-	 */
-	public boolean pool(long length) {
-		
-		if ( length > requestMaxSize) {
-			return true;
-		}
-
-		if (this.perSecondMaxSize > 0 && length > 0) {
-
-			long currentTimeMillis = TimeUtil.currentTimeMillis();
-			// long currentTimeMillis = System.currentTimeMillis();
-
-			int tempIndex = (int) ((currentTimeMillis / 1000) % 60);
-			if (currentIndex != tempIndex) {
-				synchronized (this) {
-					// 这一秒的第一条统计，把对应的存储位的数据置是 max
-					if (currentIndex != tempIndex) {
-						
-						// reset
-						sencondSizes[tempIndex].set(perSecondMaxSize);
-						currentIndex = tempIndex;
-					}
-				}
-			}
-
-			return decrement(sencondSizes[currentIndex], length) <= 0;
-		}
-
-		return true;
 	}
 
 	public String getPassword() {
@@ -75,19 +26,9 @@ public class NetFlowCfg {
 	public int getRequestMaxSize() {
 		return requestMaxSize;
 	}
-	
+
 	public boolean isControl() {
 		return isControl;
-	}
-	
-
-	private final long decrement(AtomicLong atomicLong, long delta) {
-		for (;;) {
-			long current = atomicLong.get();
-			long next = current - delta;
-			if (atomicLong.compareAndSet(current, next))
-				return next;
-		}
 	}
 
 }
