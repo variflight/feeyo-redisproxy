80369e1eda62dd76f4768008cb39250b44aa546e
beifuyiqiedeqilin <1466239199@qq.com>
2018-06-26 14:53:00 +0800

    fyi

--- src/main/java/com/feeyo/net/codec/util/CompositeByteArray.java
+++ src/main/java/com/feeyo/net/codec/util/CompositeByteArray.java
@@ -6,118 +6,103 @@ import java.util.List;
 /**
  * 封装n个物理意义的字节数组,提供逻辑意义上的字节数组
  *
- * @see: https://github.com/netty/netty/blob/4.1/buffer/src/main/java/io/netty/buffer/CompositeByteBuf.java
- * @see: https://skyao.gitbooks.io/learning-netty/content/buffer/class_CompositeByteBuf.html
+ * @see "https://github.com/netty/netty/blob/4.1/buffer/src/main/java/io/netty/buffer/CompositeByteBuf.java"
+ * @see "https://skyao.gitbooks.io/learning-netty/content/buffer/class_CompositeByteBuf.html"
  */
 public class CompositeByteArray {
-
     private List<Component> components;
-    
-    // 用于标记读取的位置
-    private int readOffset;
-    
     // 所有Component中byte[]长度之和, 用于检查下标值越界
     private int byteCount;
 
     public CompositeByteArray() {
         this.components = new ArrayList<>();
         this.byteCount = 0;
-        this.readOffset = 0;
     }
 
     public void add(byte[] bytes) {
         byteCount += bytes.length;
         int beginIndex = 0;
         int size = components.size();
+        Component prevComponent = null;
         if (size != 0) {
-            Component prevComponent = components.get(size - 1);
+            prevComponent = components.get(size - 1);
             beginIndex = prevComponent.length + prevComponent.beginIndex;
         }
- 
+
         Component comp = new Component(bytes, bytes.length, beginIndex);
         components.add(comp);
+        if (prevComponent != null) {
+            prevComponent.setNext(comp);
+        }
     }
 
     public byte get(int index) {
         Component c = findComponent(index);
-        return c.bytes[index - c.beginIndex];
+        return c.get(index);
     }
 
     // 从offset位置开始查找
-    public int firstIndex(int offset, byte value) {
-        checkIndex(offset, 1);
+    public int firstIndex(int paramOffset, byte value) {
+        checkIndex(paramOffset, 1);
 
-        Component c;
-        int indexC;
-        // 解析的请求大部分都是整包, 省略一次查找的过程
-        if (offset == 0) {
-            c = components.get(0);
-            indexC = 0;
-        } else {
-            c = findComponent(offset);
-            indexC = components.indexOf(c);
-        }
-
-        int length = components.size();
-        for (; indexC < length; indexC++) {
-
-            for (byte b : c.bytes) {
-                if (value == b) {
-                    return offset;
-                }
-                offset++;
-            }
-        }
-        return -1;
+        Component c = findComponent(paramOffset);
+        return c.find(paramOffset, value);
     }
 
-    // 取出指定区间的byte[], 可能跨多个Component
-    public byte[] subArray(int beginIndex, int length) {
-        checkIndex(beginIndex, length);
+    /**
+     * 取出指定区间的byte[], 可能跨多个Component
+     * @param beginComp beginIndex所在的Component
+     * @param beginIndex 截取的开始位置
+     * @param length 需要截取的长度
+     * @return byte[]
+     */
+    public byte[] subArray(Component beginComp, int beginIndex, int length) {
+        // checkIndex(beginIndex, length);
+        assert beginComp != null;
 
         byte[] resultArr = new byte[length];
-        Component c = findComponent(beginIndex);
-        int srcBeginIndex = beginIndex - c.beginIndex;
-        // 判断c中剩余字节是否足够
-        int availableByteCount = c.length - srcBeginIndex;
+        Component c = beginComp;
+        int remaining = length;
+        int destPos = 0;
+        int subBeginIndex = beginIndex - c.beginIndex;
+        int subLength;
 
-        // 一般都是在一个byte[]中的，这里先取一次如果不够再while循环处理
-        if (availableByteCount >= length) {
-            System.arraycopy(c.bytes, srcBeginIndex, resultArr, 0, length);
-        } else {
-            // 把c有效字节全部拷贝
-            System.arraycopy(c.bytes, srcBeginIndex, resultArr, 0, availableByteCount);
+        while (c != null && remaining > 0) {
 
-            int destPos = availableByteCount;
-            int remaining = length - availableByteCount;
-            // 利用列表和byte[]顺序存放特性取剩下的字节
-            int index = components.indexOf(c);
-
-            while (remaining > 0) {
-                c = components.get(++index);
-
-                if (remaining <= c.length) {
-                    System.arraycopy(c.bytes, 0, resultArr, destPos, remaining);
-                    break;
-                } else {
-                    System.arraycopy(c.bytes, 0, resultArr, destPos, c.length);
-                }
-
-                destPos += c.length;
-                remaining -= c.length;
+            // 表示已经不是第一个Component, 总是从0开始copy
+            if (remaining < length) {
+                subBeginIndex = 0;
+                // copy长度为Min(remaining, c.length)
+                subLength = Math.min(remaining, c.length);
+            } else {
+                // 第一个Component时, c.length - subBeginIndex 为Component中剩余有效字节数
+                subLength = Math.min(remaining, c.length - subBeginIndex);
             }
+
+            System.arraycopy(c.bytes, subBeginIndex, resultArr, destPos, subLength);
+            remaining = remaining - subLength;
+            destPos = destPos + subLength;
+            c = c.next;
         }
 
         return resultArr;
     }
 
+    public byte[] subArray(int beginIndex, int length) {
+        Component c = findComponent(beginIndex);
+        return subArray(c, beginIndex, length);
+    }
+
+    /**
+     * 返回剩余可读字节数
+     */
+    public int remaining(int readOffset) {
+        return Math.max(byteCount - readOffset, 0);
+    }
 
     public int getByteCount() {
         return byteCount;
     }
-
-    public int getReadOffset() { return this.readOffset; }
-    public void setReadOffset(int offset) { this.readOffset = offset; }
 
     /**
      * 清空其管理的所有byte[]并重置index <br>
@@ -125,11 +110,11 @@ public class CompositeByteArray {
     public void clear() {
         components.clear();
         byteCount = 0;
-        readOffset = 0;
     }
 
-    private Component findComponent(int offset) {
-        checkIndex(offset);
+    public Component findComponent(int offset) {
+        // 依赖外部调用检查
+        // checkIndex(offset, 1);
 
         // 二分查找
         for (int low = 0, high = components.size(); low <= high; ) {
@@ -145,35 +130,75 @@ public class CompositeByteArray {
             }
         }
 
-        throw new Error("should not reach here");
+        throw new IndexOutOfBoundsException("Not enough data.");
     }
 
-    private void checkIndex(int index) {
-        checkIndex(index, 1);
-    }
-    
     private void checkIndex(int index, int length) {
-        if (isOutOfBounds(index, length, byteCount)) {
+        if ((index | length | (index + length) | (byteCount - (index + length))) < 0) {
             throw new IndexOutOfBoundsException(String.format("index: %d, length: %d (expected: range(0, %d))", index, length,
                     byteCount));
         }
     }
 
-    private boolean isOutOfBounds(int index, int length, int capacity) {
-        return (index | length | (index + length) | (capacity - (index + length))) < 0;
-    }
-
     // 包装了 byte[], 增加了 length 和 beginIndex 方便查找
-    private final class Component {
+    public final class Component {
         final byte[] bytes;
         final int length;
         final int beginIndex;
+        // 优化遍历, 维护单向链表
+        private Component next;
 
         public Component(byte[] bytes, int length, int endIndex) {
             this.bytes = bytes;
             this.length = length;
             this.beginIndex = endIndex;
         }
-    }
 
+        public Component getNext() {
+            return next;
+        }
+
+        public void setNext(Component next) {
+            this.next = next;
+        }
+
+        public boolean isInComponent(int index) {
+            return index < beginIndex + length && index >= beginIndex;
+        }
+
+        public byte get(int index) {
+            return bytes[index - beginIndex];
+        }
+
+        // 获取index下一个字节
+        public byte getNextByte(int index) {
+            int actualIndex = index + 1;
+            if (actualIndex - beginIndex < length) {
+                return bytes[actualIndex - beginIndex];
+            }
+            else {
+                return next.get(actualIndex);
+            }
+        }
+
+        // Component内部实现查找方法
+        public int find(int index, byte value) {
+            // check index 有效性
+            Component c = this;
+
+            // 利用链表和数组快速查找
+            while (c != null) {
+
+                while (index < c.beginIndex + c.length) {
+
+                    if (value == c.get(index)) {
+                        return index;
+                    }
+                    index++;
+                }
+                c = c.next;
+            }
+            return -1;
+        }
+    }
 }
