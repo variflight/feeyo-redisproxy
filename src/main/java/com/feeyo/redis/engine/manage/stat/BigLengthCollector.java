98cd77efa998cdb1ac9905986e35490f1df7fa9a
dsliu <liudongsong@variflight.com>
2019-05-21 12:49:04 +0800

    rt 分组发送 数据

--- src/main/java/com/feeyo/redis/engine/manage/stat/BigLengthCollector.java
+++ src/main/java/com/feeyo/redis/engine/manage/stat/BigLengthCollector.java
@@ -11,233 +11,251 @@ import com.feeyo.util.jedis.RedisCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BigLengthCollector implements StatCollector {
-	
-	private static Logger LOGGER = LoggerFactory.getLogger( BigLengthCollector.class );
-	
-	private final static int THRESHOLD = 10000;
-	
-	private final static int SIZE_OF_1K = 1024;
-	private final static int SIZE_OF_10K = 10 * SIZE_OF_1K;
-	
-	
-	// key -> password,cmd
-	private static ConcurrentHashMap<String, String[]> keyMap = new ConcurrentHashMap<String, String[]>();
-	
-	private static ConcurrentHashMap<String, BigLength> bigLengthMap = new ConcurrentHashMap<String, BigLength>();
-	
-	private static long lastCheckTime = TimeUtil.currentTimeMillis();
-	private static AtomicBoolean isChecking = new AtomicBoolean(false);
+
+    private static Logger LOGGER = LoggerFactory.getLogger(BigLengthCollector.class);
+
+    private final static int THRESHOLD = 10000;
+
+    private final static int SIZE_OF_1K = 1024;
+    private final static int SIZE_OF_10K = 10 * SIZE_OF_1K;
 
 
+    // key -> password,cmd
+    private static ConcurrentHashMap<String, String[]> keyMap = new ConcurrentHashMap<String, String[]>();
 
-	/**
-	 * 检查 redis key
-	 */
-	private void checkListKeyLength() {
-		
-		if ( !isChecking.compareAndSet(false, true) ) {
-			return;
-		}
-		
-		try {
+    private static ConcurrentHashMap<String, BigLength> bigLengthMap = new ConcurrentHashMap<String, BigLength>();
 
-			lastCheckTime = TimeUtil.currentTimeMillis();
+    private static long lastCheckTime = TimeUtil.currentTimeMillis();
+    private static AtomicBoolean isChecking = new AtomicBoolean(false);
 
-            Set<String> connectionExceptionIps = new HashSet<>();
-            // key -> password & cmd
-			for (java.util.Map.Entry<String, String[]>  entry : keyMap.entrySet()) {
-				
-				String key = entry.getKey();
-				String[] value = entry.getValue();
-				//
-				String password = value[0];
-				String cmd = value[1];
-				
-				UserCfg userCfg = RedisEngineCtx.INSTANCE().getUserMap().get( password );
-				if (userCfg != null) {
-					//
-					AbstractPool pool = RedisEngineCtx.INSTANCE().getPoolMap().get( userCfg.getPoolId() );
-					PhysicalNode physicalNode = (pool.getType() == PoolType.REDIS_CLUSTER ? pool.getPhysicalNode(cmd, key) : pool.getPhysicalNode());
-                    if (physicalNode == null) {
-                        continue;
-                    }
 
-                    String ip = physicalNode.getHost() + ":" + physicalNode.getPort();
-                    //连接异常IP 其他需要连接到这个IP的 本周期内不连接
-                    if (connectionExceptionIps.contains(ip)) {
-                        continue;
-                    }
+    /**
+     * 检查 redis key
+     */
+    private void checkListKeyLength() {
 
-					JedisConnection conn = null;
-					try {
-
-						// 前置设置 readonly
-						conn = new JedisConnection(physicalNode.getHost(), physicalNode.getPort(), 1000, 0);
-
-						if (cmd.equals("HMSET") || 
-								cmd.equals("HSET") || 
-								cmd.equals("HSETNX") || 
-								cmd.equals("HINCRBY") || 
-								cmd.equals("HINCRBYFLOAT")) { // hash
-
-							conn.sendCommand(RedisCommand.HLEN, key);
-
-						} else if (cmd.equals("LPUSH") || 
-								cmd.equals("LPUSHX") || 
-								cmd.equals("RPUSH") || 
-								cmd.equals("RPUSHX")) { // list
-
-							conn.sendCommand(RedisCommand.LLEN, key);
-
-						} else if (cmd.equals("SADD")) { // set
-
-							conn.sendCommand(RedisCommand.SCARD, key);
-
-						} else if (cmd.equals("ZADD") || 
-								   cmd.equals("ZINCRBY") || 
-								   cmd.equals("ZREMRANGEBYLEX")) { // sortedset
-
-							conn.sendCommand(RedisCommand.ZCARD, key);
-						}
-
-						// 获取集合长度
-						long length = conn.getIntegerReply();
-						if (length > THRESHOLD) {
-
-							BigLength bigLen = bigLengthMap.get(key);
-							if (bigLen == null) {
-								bigLen = new BigLength();
-								bigLen.cmd = cmd;
-								bigLen.key = key;
-								bigLen.password = password;
-								bigLengthMap.put(key, bigLen);
-							}
-							bigLen.length.set((int) length);
-
-						} else {
-							bigLengthMap.remove(key);
-						}
-
-						keyMap.remove(key);
-
-						// ###########################################
-						if (bigLengthMap.size() > 100) {
-							BigLength min = null;
-							for (BigLength bigLen : bigLengthMap.values()) {
-								if (min == null) {
-									min = bigLen;
-								} else {
-									if (bigLen.length.get() < min.length.get()) {
-										min = bigLen;
-									}
-								}
-							}
-							bigLengthMap.remove(min.key);
-						}
-
-					} catch (Exception e2) {
-						connectionExceptionIps.add(ip);
-						LOGGER.error("check big length err:", e2);
-					} finally {
-						if (conn != null) {
-							conn.disconnect();
-						}
-					}
-					
-				}
-			}
-			
-			
-		} finally {
-			isChecking.set(false);
-		}
-	}
-
-	    
-	public ConcurrentHashMap<String, BigLength> getBigLengthMap() {
-		return bigLengthMap;
-	}
-	
-	
-
-	
-	@Override
-	public void onCollect(String password, String cmd, String key, int requestSize, int responseSize, 
-			int procTimeMills, int waitTimeMills, boolean isCommandOnly, boolean isBypass ) {
-
-		// 统计集合类型key
-		if (  	cmd.equals("HMSET") 	// hash
-				|| cmd.equals("HSET") 	
-				|| cmd.equals("HSETNX") 	
-				|| cmd.equals("HINCRBY") 	
-				|| cmd.equals("HINCRBYFLOAT") 	
-				
-				|| cmd.equals("LPUSH") 	// list
-				|| cmd.equals("LPUSHX") 
-				|| cmd.equals("RPUSH") 
-				|| cmd.equals("RPUSHX") 
-				
-				|| cmd.equals("SADD") // set
-				
-				|| cmd.equals("ZADD")  // sortedset
-				|| cmd.equals("ZINCRBY") 
-				|| cmd.equals("ZREMRANGEBYLEX") ) {
-		
-	
-				BigLength bigLength = bigLengthMap.get(key);
-				if (bigLength != null) {
-					if (requestSize > SIZE_OF_10K ) {
-						bigLength.count_10k.incrementAndGet();
-						
-					} else if (requestSize > SIZE_OF_1K) {
-						bigLength.count_1k.incrementAndGet();
-					}
-				}
-				
-				//
-				if ( !keyMap.containsKey(key) ) {
-					
-					if (keyMap.size() < 1000) {;
-						keyMap.put(key, new String[]{ password, cmd});
-					} else {
-						checkListKeyLength();
-					}
-				}
-		}
-		
-	}
-	
-	
-	@Override
-	public void onScheduleToZore() {
-		ConcurrentHashMap<String, String[]> tmp = new ConcurrentHashMap<String, String[]>();
-		for (BigLength bigLength : bigLengthMap.values()) { 
-			tmp.put(bigLength.key, new String[] { bigLength.password, bigLength.cmd} );
-		}
-		
-		keyMap.putAll(tmp);
-	}
-	
-	@Override
-	public void onSchedulePeroid(int peroid) {
-		if (TimeUtil.currentTimeMillis() - lastCheckTime >= peroid * 1000 * 10 ) {
-			checkListKeyLength();
+        if (!isChecking.compareAndSet(false, true)) {
+            return;
         }
-	}
-	
-	public static class BigLength {
-		public String password;
-		public String cmd;
-		public String key;
-		public AtomicInteger length = new AtomicInteger(0);
-		public AtomicInteger count_1k = new AtomicInteger(0);
-		public AtomicInteger count_10k = new AtomicInteger(0);
-	}
+
+        try {
+
+            lastCheckTime = TimeUtil.currentTimeMillis();
+
+            Map<PhysicalNode, List<String[]>> toCheckKeys = new HashMap<>(14);
+
+            // 按照物理节点分组 然后发送 验证
+            for (java.util.Map.Entry<String, String[]> entry : keyMap.entrySet()) {
+
+                String key = entry.getKey();
+                String[] value = entry.getValue();
+
+                String password = value[0];
+                String cmd = value[1];
+
+                UserCfg userCfg = RedisEngineCtx.INSTANCE().getUserMap().get(password);
+                if (userCfg == null) {
+                    continue;
+                }
+
+                AbstractPool pool = RedisEngineCtx.INSTANCE().getPoolMap().get(userCfg.getPoolId());
+                PhysicalNode physicalNode = (pool.getType() == PoolType.REDIS_CLUSTER ? pool.getPhysicalNode(cmd, key) : pool.getPhysicalNode());
+                if (physicalNode == null) {
+                    continue;
+                }
+
+                List<String[]> keyAndCmdList = toCheckKeys.get(physicalNode);
+                if (keyAndCmdList == null) {
+                    keyAndCmdList = new ArrayList<>();
+                    toCheckKeys.put(physicalNode, keyAndCmdList);
+                }
+
+                String[] keyAndCmd = new String[]{key, cmd};
+                keyAndCmdList.add(keyAndCmd);
+            }
+
+            for (java.util.Map.Entry<PhysicalNode, List<String[]>> entry : toCheckKeys.entrySet()) {
+
+                PhysicalNode physicalNode = entry.getKey();
+                List<String[]> keyAndCmdList = entry.getValue();
+
+                    JedisConnection conn = null;
+                    try {
+                        // 前置设置 readonly
+                        conn = new JedisConnection(physicalNode.getHost(), physicalNode.getPort(), 1000, 1000);
+
+                        for (String[] strings: keyAndCmdList) {
+
+                            String key = strings[0];
+                            String cmd = strings[1];
+
+                            if (cmd.equals("HMSET") ||
+                                    cmd.equals("HSET") ||
+                                    cmd.equals("HSETNX") ||
+                                    cmd.equals("HINCRBY") ||
+                                    cmd.equals("HINCRBYFLOAT")) { // hash
+
+                                conn.sendCommand(RedisCommand.HLEN, key);
+
+                            } else if (cmd.equals("LPUSH") ||
+                                    cmd.equals("LPUSHX") ||
+                                    cmd.equals("RPUSH") ||
+                                    cmd.equals("RPUSHX")) { // list
+
+                                conn.sendCommand(RedisCommand.LLEN, key);
+
+                            } else if (cmd.equals("SADD")) { // set
+
+                                conn.sendCommand(RedisCommand.SCARD, key);
+
+                            } else if (cmd.equals("ZADD") ||
+                                    cmd.equals("ZINCRBY") ||
+                                    cmd.equals("ZREMRANGEBYLEX")) { // sortedset
+
+                                conn.sendCommand(RedisCommand.ZCARD, key);
+                            }
+
+                            // 获取集合长度
+                            long length = conn.getIntegerReply();
+                            if (length > THRESHOLD) {
+
+                                BigLength bigLen = bigLengthMap.get(key);
+                                if (bigLen == null) {
+                                    bigLen = new BigLength();
+                                    bigLen.cmd = cmd;
+                                    bigLen.key = key;
+                                    String[] value = keyMap.get(key);
+                                    if (value != null) {
+                                        bigLen.password = value[0];
+                                        bigLengthMap.put(key, bigLen);
+                                    }
+                                }
+                                bigLen.length.set((int) length);
+
+                            } else {
+                                bigLengthMap.remove(key);
+                            }
+
+                            keyMap.remove(key);
+
+                            // ###########################################
+                            if (bigLengthMap.size() > 100) {
+                                BigLength min = null;
+                                for (BigLength bigLen : bigLengthMap.values()) {
+                                    if (min == null) {
+                                        min = bigLen;
+                                    } else {
+                                        if (bigLen.length.get() < min.length.get()) {
+                                            min = bigLen;
+                                        }
+                                    }
+                                }
+                                bigLengthMap.remove(min.key);
+                            }
+                        }
+                    } catch (Exception e2) {
+                        LOGGER.error("check big length err:", e2);
+                    } finally {
+                        if (conn != null) {
+                            conn.disconnect();
+                        }
+                        keyAndCmdList.clear();
+                    }
+
+            }
+
+            toCheckKeys.clear();
+        } finally {
+            isChecking.set(false);
+        }
+    }
+
+
+    public ConcurrentHashMap<String, BigLength> getBigLengthMap() {
+        return bigLengthMap;
+    }
+
+
+    @Override
+    public void onCollect(String password, String cmd, String key, int requestSize, int responseSize,
+                          int procTimeMills, int waitTimeMills, boolean isCommandOnly, boolean isBypass) {
+
+        // 统计集合类型key
+        if (cmd.equals("HMSET")    // hash
+                || cmd.equals("HSET")
+                || cmd.equals("HSETNX")
+                || cmd.equals("HINCRBY")
+                || cmd.equals("HINCRBYFLOAT")
+
+                || cmd.equals("LPUSH")    // list
+                || cmd.equals("LPUSHX")
+                || cmd.equals("RPUSH")
+                || cmd.equals("RPUSHX")
+
+                || cmd.equals("SADD") // set
+
+                || cmd.equals("ZADD")  // sortedset
+                || cmd.equals("ZINCRBY")
+                || cmd.equals("ZREMRANGEBYLEX")) {
+
+
+            BigLength bigLength = bigLengthMap.get(key);
+            if (bigLength != null) {
+                if (requestSize > SIZE_OF_10K) {
+                    bigLength.count_10k.incrementAndGet();
+
+                } else if (requestSize > SIZE_OF_1K) {
+                    bigLength.count_1k.incrementAndGet();
+                }
+            }
+
+            //
+            if (!keyMap.containsKey(key)) {
+
+                if (keyMap.size() < 1000) {
+                    ;
+                    keyMap.put(key, new String[]{password, cmd});
+                } else {
+                    checkListKeyLength();
+                }
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onScheduleToZore() {
+        ConcurrentHashMap<String, String[]> tmp = new ConcurrentHashMap<String, String[]>();
+        for (BigLength bigLength : bigLengthMap.values()) {
+            tmp.put(bigLength.key, new String[]{bigLength.password, bigLength.cmd});
+        }
+
+        keyMap.putAll(tmp);
+    }
+
+    @Override
+    public void onSchedulePeroid(int peroid) {
+        if (TimeUtil.currentTimeMillis() - lastCheckTime >= peroid * 1000 * 10) {
+            checkListKeyLength();
+        }
+    }
+
+    public static class BigLength {
+        public String password;
+        public String cmd;
+        public String key;
+        public AtomicInteger length = new AtomicInteger(0);
+        public AtomicInteger count_1k = new AtomicInteger(0);
+        public AtomicInteger count_10k = new AtomicInteger(0);
+    }
 }
