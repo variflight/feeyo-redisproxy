6267d4be8929432203d38c899727ec2f2aa4e4d2
zhuam <zhuam@foxmail.com>
2019-05-24 16:59:39 +0800

    rt

--- src/main/java/com/feeyo/redis/engine/manage/stat/BigKeyCollector.java
+++ src/main/java/com/feeyo/redis/engine/manage/stat/BigKeyCollector.java
@@ -9,226 +9,221 @@ import java.util.concurrent.atomic.AtomicLong;
 
 // 
 public class BigKeyCollector implements StatCollector {
-    public static int LENGTH = 500;
+	public static int LENGTH = 500;
 
-    // required size
-    public static volatile int REQUIRED_SIZE = 1024 * 256;
+	// required size
+	public static volatile int REQUIRED_SIZE = 1024 * 256;
 
+	private ConcurrentHashMap<String, BigKey> bkHashMap = new ConcurrentHashMap<String, BigKey>(); // use
+																									// search
 
-    private ConcurrentHashMap<String, BigKey> bkHashMap = new ConcurrentHashMap<String, BigKey>();	// use search
+	private AtomicLong bkSize = new AtomicLong(0);
 
+	// bigkey 总数及旁路数
+	private AtomicLong totalCount = new AtomicLong(0);
+	private AtomicLong bypassCount = new AtomicLong(0);
 
-    private AtomicLong bkSize = new AtomicLong(0);
+	public void setSize(int size) {
+		REQUIRED_SIZE = size;
+	}
 
-    //bigkey 总数及旁路数
-    private AtomicLong totalCount = new AtomicLong(0);
-    private AtomicLong bypassCount = new AtomicLong(0);
+	//
+	public List<BigKey> getTop100() {
 
-    public void setSize(int size) {
-            REQUIRED_SIZE = size;
-    }
+		try {
 
-    private static final int HUNDRED=100;
+			List<BigKey> newList = new ArrayList<>( 100 );
 
-    public List<BigKey> getTop100() {
+			Collection<BigKey> bigKeySet = bkHashMap.values();
+			if (bigKeySet == null || bigKeySet.isEmpty()) {
+				return newList;
+			}
 
-        		try {
+			// 排序
+			List<BigKey> keyList = new ArrayList<>(bigKeySet);
+			Collections.sort(keyList, new BigKeyComparator());
+			if (keyList.size() <= 100) {
+				return keyList;
+			}
 
-                    List<BigKey> newList = new ArrayList<>( HUNDRED );
+			newList = keyList.subList(0, 100);
+			return newList;
 
-                    Collection<BigKey> bigKeySet = bkHashMap.values();
-                    if (bigKeySet == null || bigKeySet.isEmpty()) {
-                        return newList;
-                    }
+		} finally {
 
-                    //排序
-                    List<BigKey> keyList = new ArrayList<>(bigKeySet);
-                    Collections.sort(keyList, new BigKey());
-                    if (keyList.size() <= HUNDRED) {
-                        return keyList;
-                    }
+		}
 
-                    newList = keyList.subList(0, HUNDRED);
-        			return newList ;
+	}
 
-        		} finally {
+	@Override
+	public void onCollect(String password, String cmd, String key, int requestSize, int responseSize, int procTimeMills,
+			int waitTimeMills, boolean isCommandOnly, boolean isBypass) {
 
-        		}
+		if (isCommandOnly)
+			return;
 
-    }
+		// check size
+		if (requestSize < REQUIRED_SIZE && responseSize < REQUIRED_SIZE) {
+			return;
+		}
 
-    @Override
-    public void onCollect(String password, String cmd, String key, int requestSize, int responseSize,
-                          int procTimeMills, int waitTimeMills, boolean isCommandOnly, boolean isBypass) {
+		if (isBypass) 
+			bypassCount.incrementAndGet();
+		//
+		totalCount.incrementAndGet();
 
-        if ( isCommandOnly )
-            return;
+		// 判断是否存在 big key map
+		BigKey bk = bkHashMap.get(key);
+		if (bk == null) {
+			// 不存在 判断是否超过 bk size
+			if (bkSize.get() > LENGTH) {
+				// 找到最小值 然后remove
+				removeMinBigKey();
+			}
+			bk = new BigKey();
+			bk.key = key;
+			bk.lastCmd = cmd;
+			bk.size = requestSize > responseSize ? requestSize : responseSize;
+			bk.lastUseTime = TimeUtil.currentTimeMillis();
+			bk.count.incrementAndGet();
+			bk.fromReq = requestSize >= REQUIRED_SIZE;
+			bk.fromResp = responseSize >= REQUIRED_SIZE;
+			bkSize.incrementAndGet();
+			// 添加
+			bkHashMap.put(key, bk);
+		} else {
+			bk.key = key;
+			bk.lastCmd = cmd;
+			bk.size = requestSize > responseSize ? requestSize : responseSize;
+			bk.lastUseTime = TimeUtil.currentTimeMillis();
+			bk.count.incrementAndGet();
+			bk.fromReq = requestSize >= REQUIRED_SIZE;
+			bk.fromResp = responseSize >= REQUIRED_SIZE;
+		}
 
-        //  check size
-        if (requestSize < REQUIRED_SIZE && responseSize < REQUIRED_SIZE) {
-            return;
-        }
+	}
 
-        if (isBypass) {
-            bypassCount.incrementAndGet();
-        }
+	@Override
+	public void onScheduleToZore() {
 
-        //总数+1
-        totalCount.incrementAndGet();
+		bkHashMap.clear();
+		totalCount.set(0);
+		bypassCount.set(0);
+		bkSize.set(0);
+	}
 
+	@Override
+	public void onSchedulePeroid(int peroid) {
+		// ignore
+	}
 
-        BigKey bk = bkHashMap.get(key);
-
-        //判断是否存在 big key map
-        if (bk == null) {
-            //不存在 判断是否超过 bk size
-            if (bkSize.get() > LENGTH) {
-                //找到最小值 然后remove
-                removeMinBigKey();
-            }
-            bk=new BigKey();
-            bk.key=key;
-            bk.lastCmd = cmd;
-            bk.size = requestSize > responseSize ? requestSize : responseSize;
-            bk.lastUseTime = TimeUtil.currentTimeMillis();
-            bk.count.incrementAndGet();
-            bk.fromReq = requestSize >= REQUIRED_SIZE;
-            bk.fromResp = responseSize >= REQUIRED_SIZE;
-            bkSize.incrementAndGet();
-            //添加
-            bkHashMap.put(key,bk);
-        }else {
-            bk.key = key;
-            bk.lastCmd = cmd;
-            bk.size = requestSize > responseSize ? requestSize : responseSize;;
-            bk.lastUseTime = TimeUtil.currentTimeMillis();
-            bk.count.incrementAndGet();
-            bk.fromReq = requestSize >= REQUIRED_SIZE;
-            bk.fromResp = responseSize >= REQUIRED_SIZE;
-        }
-
-    }
-
-    @Override
-    public void onScheduleToZore() {
-
-        bkHashMap.clear();
-        totalCount.set(0);
-        bypassCount.set(0);
-        bkSize.set(0);
-    }
-
-    @Override
-    public void onSchedulePeroid(int peroid) {
-        // ignore
-    }
-
-
-    public boolean isResponseBigkey(String cmd, String key) {
+	public boolean isResponseBigkey(String cmd, String key) {
 
 		/*
-			后续需要对 指令类别做精细化控制
-			如： HGETALL key  是慢查询， 但  HGET key field 则不是的情况
+		 * 后续需要对 指令类别做精细化控制 如： HGETALL key 是慢查询， 但 HGET key field 则不是的情况
 		 */
 
-        BigKey bk = bkHashMap.get(key);
-        if (bk != null && bk.fromResp && bk.lastCmd.equals(cmd) ) {
-            return true;
-        }
-        return false;
-    }
+		BigKey bk = bkHashMap.get(key);
+		if (bk != null && bk.fromResp && bk.lastCmd.equals(cmd)) {
+			return true;
+		}
+		return false;
+	}
 
-    public void deleteResponseBigkey(String key) {
-        bypassCount.incrementAndGet();
-        totalCount.incrementAndGet();
-        bkHashMap.remove(key);
-        bkSize.decrementAndGet();
-    }
+	public void deleteResponseBigkey(String key) {
+		bypassCount.incrementAndGet();
+		totalCount.incrementAndGet();
+		bkHashMap.remove(key);
+		bkSize.decrementAndGet();
+	}
 
-    public long getBigKeyCount() {
-        return totalCount.get();
-    }
+	public long getBigKeyCount() {
+		return totalCount.get();
+	}
 
-    public long getBypassBigKeyCount() {
-        return bypassCount.get();
-    }
+	public long getBypassBigKeyCount() {
+		return bypassCount.get();
+	}
 
-    private void removeMinBigKey() {
-        Collection<BigKey> bigKeySet = bkHashMap.values();
-        if (bigKeySet == null || bigKeySet.isEmpty()) {
-            return;
-        }
-        if (bkSize.get() < LENGTH) {
-            return;
-        }
+	private void removeMinBigKey() {
+		Collection<BigKey> bigKeySet = bkHashMap.values();
+		if (bigKeySet == null || bigKeySet.isEmpty()) {
+			return;
+		}
+		if (bkSize.get() < LENGTH) {
+			return;
+		}
 
-        //排序
-        List<BigKey> keyList = new ArrayList<>(bigKeySet);
-        Collections.sort(keyList, new BigKey());
+		// 排序
+		List<BigKey> keyList = new ArrayList<>(bigKeySet);
+		Collections.sort(keyList, new BigKeyComparator());
 
-        int size = keyList.size();
-        BigKey minKey = keyList.get(size - 1);
-        int minCount = minKey.count.get();
-        for (int i = size - 1; i > 400; i--) {
-            BigKey bk = keyList.get(i);
-            if (bk != null && bk.count.get() == minCount) {
-                bkSize.decrementAndGet();
-                bkHashMap.remove(bk.key);
-            }
-        }
-        keyList.clear();
-    }
+		int size = keyList.size();
+		BigKey minKey = keyList.get(size - 1);
+		int minCount = minKey.count.get();
+		for (int i = size - 1; i > 400; i--) {
+			BigKey bk = keyList.get(i);
+			if (bk != null && bk.count.get() == minCount) {
+				bkSize.decrementAndGet();
+				bkHashMap.remove(bk.key);
+			}
+		}
+		keyList.clear();
+	}
 
-    //
-    public static class BigKey implements Comparator<BigKey> {
+	
+	public static class BigKeyComparator implements Comparator<BigKey> {
+		
+		public static boolean isASC = false;
 
-        public static boolean isASC = false;
+		@Override
+		public int compare(BigKey o1, BigKey o2) {
 
-        public String lastCmd;
-        public String key;
-        public int size;
-        public AtomicInteger count = new AtomicInteger(1);
-        public boolean fromReq;
-        public boolean fromResp;
-        public long lastUseTime;
+			if (o1 == null || o2 == null) {
+				return -1;
+			}
 
-        @Override
-        public boolean equals(Object obj) {
-            if(obj == this)
-                return true;
+			long a, b;
+			if (isASC) {
+				a = o1.count.get();
+				b = o2.count.get();
+			} else {
+				a = o2.count.get();
+				b = o1.count.get();
+			}
 
-            if(obj == null || !(obj instanceof BigKey))
-                return false;
+			if (a > b)
+				return 1; // 大于
+			else if (a == b)
+				return 0; // 等于
+			else
+				return -1; // 小于
+		}
+		
+	}
+	
+	//
+	public static class BigKey extends BigKeyComparator {
 
-            BigKey bigkey = (BigKey) obj;
-            return this.key.equals(bigkey.key);
-        }
+		public String lastCmd;
+		public String key;
+		public int size;
+		public AtomicInteger count = new AtomicInteger(1);
+		public boolean fromReq;
+		public boolean fromResp;
+		public long lastUseTime;
 
-        @Override
-        public int compare(BigKey o1, BigKey o2) {
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == this)
+				return true;
 
-            if ( o1 == null || o2 == null ) {
-                return -1;
-            }
+			if (obj == null || !(obj instanceof BigKey))
+				return false;
 
-            long a, b;
-            if ( isASC ) {
-                a = o1.count.get();
-                b = o2.count.get();
-            } else {
-                a = o2.count.get();
-                b = o1.count.get();
-            }
-
-            if (a > b)
-                return 1;	// 大于
-            else if (a == b)
-                return 0;	//等于
-            else
-                return -1;	//小于
-        }
-
-    }
-
+			BigKey bigkey = (BigKey) obj;
+			return this.key.equals(bigkey.key);
+		}
+	}
 
 }
